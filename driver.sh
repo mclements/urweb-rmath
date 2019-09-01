efaa542f17737528e2fc76359bbd434220f040ae
Mark Clements <mark.clements@ki.se>
2019-09-01 17:08:39 +0200

    Rename cffi -> test

---
+++ driver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Supply at least one argument'
+    exit 1
+fi
+
+TESTPID=/tmp/$1.pid
+TESTSRV=./$1.exe
+
+rm -f $TESTPID $TESTSRV
+urweb -debug -boot -noEmacs "$1" || exit 1
+
+$TESTSRV -q -a 127.0.0.1 &
+echo $! >> $TESTPID
+sleep 1
+## wget http://localhost:8080/Cffi/main -O -
+firefox --new-tab http://localhost:8080/Cffi/main
+sleep 1
+kill `cat $TESTPID`
