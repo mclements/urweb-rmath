f28478f1e7fee6397fb08e8d1a09fbb9000de346
Mark Clements <mark.clements@ki.se>
2020-05-17 17:20:12 +0200

    Add chartjsArgs Javascript function

--- chartjsffi.js
+++ chartjsffi.js
@@ -70,20 +70,26 @@ function objectifyUr(urValue, newKeyMap) {
 	else if (Object.keys(ur)[0] == 'v') {
     	    return convert(ur.v);
 	}
-	// case: Some ??
+	// case: Type constructor
 	else if (Object.keys(ur)[0] == 'n' && Object.keys(ur)[1] == 'v') {
-    	    return convert(ur.v);
+	    var d = {};
+	    var key = '_'+ur.n;
+	    d[key] = convert(ur.v);
+    	    return convert(d);
 	}
 	// case: Ur/Web struct
 	else {
 	    return Object.keys(ur).reduce(function(result, key) {
-		var newKey = key.substr(1,1).toLowerCase()+key.substr(2);
 		var y = convert(ur[key]);
+		var newKey = key;
+		if (key.substr(0,1)=='_') {
+		    newKey = key.substr(1,1).toLowerCase()+key.substr(2);
+		}
 		if (Object.keys(newKeyMap).includes(newKey)) {
 		    newKey = newKeyMap[newKey];
 		}
 		if (y != null) {
-		    result[newKey] = convert(ur[key]);
+		    result[newKey] = y;
 		}
 		return result;
 	    }, {});
@@ -92,19 +98,61 @@ function objectifyUr(urValue, newKeyMap) {
     return convert(urValue, newKeyMap);
 }
 
+function chartjsArgs(obj) {
+    function convert(obj) {
+        function array2object(a) {
+            var acc = {};
+            var i;
+            for(i=0; i<a.length; i++) {
+                var key = Object.keys(a[i])[0];
+                var y = a[i][key];
+                // arguments that are part of a list
+                if (key=='data' && Array.isArray(y)) {
+                    // unchanged
+                }
+                else if (key=='legend' && Array.isArray(y)) {
+		    // nested arguments
+                    y = array2object(y);
+                }
+                else y = convert(y);
+            acc[key] =  y;
+            }
+            return acc;
+        }
+        if (obj == null)
+	        return null;
+        else if (typeof(obj) != 'object') // atom
+            return obj;
+        else
+            return Object.keys(obj).reduce(function(result, key) {
+                var y = obj[key];
+                // arguments that are part of a record
+                if (key=='datasets' && Array.isArray(y))
+                    y = y.map(array2object); // array of arrays -> array of objects
+                else if (key=='options' && Array.isArray(y))
+                    y = array2object(y); // array -> object
+                else y = convert(y);
+                result[key] = y;
+                return result;
+            }, {});
+    }
+    return convert(obj);
+};
+
 function chartjsChartStruct(id, obj) {
 
     var ctx = document.getElementById(id).getContext('2d');
 
-    var chart = new Chart(ctx, objectifyUr(obj, urNewKeyMap));
+    var chart = new Chart(ctx, chartjsArgs(objectifyUr(obj, urNewKeyMap)));
     
     return chart;
 }
 
 function chartjsChartStructDebug(id, obj) {
 
-    alert(JSON.stringify(obj));
-    // alert(JSON.stringify(objectifyUr(obj)));
+    // alert(JSON.stringify(obj));
+    alert(JSON.stringify(objectifyUr(obj,urNewKeyMap)));
+    alert(JSON.stringify(chartjsArgs(objectifyUr(obj,urNewKeyMap))));
     
     return chartjsChartStruct(id,obj);
 }
